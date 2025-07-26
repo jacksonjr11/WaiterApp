8ff720a6168364491830d5d1cd4265c67c239598
Jackson Augusto <jackson.augusto.1571@gmail.com>
2025-07-26 08:54:01 -0300

    Create find and create category use case

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
+import { router } from "./router";
 
-const app = express();
+mongoose.connect('mongodb://localhost:27017').then(() => {
+    const app = express();
+    const port = 3000;
 
-mongoose.connect('mongodb://localhost:27017').then(() => console.log('connected mongo db')).catch(() => console.log('Error connect mongo db'));
+    app.use(express.json());
+    app.use(router);
+    
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+})
+.catch(() => console.log('Error connect mongo db'));
 
-app.listen(3000, () => {
-    console.log('Server is running')
-});
