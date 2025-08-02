c598930793f85602459885c325c8b1c5e5d519af
Jackson Augusto <jackson.augusto.1571@gmail.com>
2025-08-02 17:56:48 -0300

    Created Orders Component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import {Header} from "./Header";
+import { Orders } from "./Orders";
 
 export default function App() {
   return (
     <React.Fragment>
       <GlobalStyles />
       <Header />
+      <Orders />
     </React.Fragment>
   );
 }
